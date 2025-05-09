ad3ccd4db01d1c40e8bb80e049d371de28f48122
Adel Ahmetgaliev <adel.ahmetgaliev@yandex.ru>
2025-05-09 12:13:33 +0300

    Added new colors

--- include/colors.h
+++ include/colors.h
@@ -5,6 +5,8 @@
 
 const Clay_Color CLAY_COLOR_BLACK = (Clay_Color) { 0, 0, 0, 255 };
 const Clay_Color CLAY_COLOR_WHITE = (Clay_Color) { 255, 255, 255, 255 };
+const Clay_Color CLAY_COLOR_DARKEST_GREY = (Clay_Color) { 10, 10, 10, 255 };
+const Clay_Color CLAY_COLOR_DARKER_GREY = (Clay_Color) { 30, 30, 30, 255 };
 const Clay_Color CLAY_COLOR_DARK_GREY = (Clay_Color) { 50, 50, 50, 255 };
 const Clay_Color CLAY_COLOR_GREY = (Clay_Color) { 70, 70, 70, 255 };
 const Clay_Color CLAY_COLOR_LIGHT_GREY = (Clay_Color) { 90, 90, 90, 255 };
